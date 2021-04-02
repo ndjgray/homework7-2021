c9dae00305e38614a22dbf728f55da65a953e778
ndjgray <60194985+ndjgray@users.noreply.github.com>
2021-04-01 22:53:37 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,72 @@
+var video = document.getElementById("player1");
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow Down");
+	video.playbackRate *= .9;
+	console.log(video.playbackRate)
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Speed Up");
+	video.playbackRate /= .9;
+	console.log(video.playbackRate)
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log("Skip Ahead");
+	if (video.currentTime < video.duration - 15){
+		video.currentTime += 15;
+	}
+	else{
+		video.currentTime = 0;
+	}
+	console.log(video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log("Muted");
+	if (video.muted == false){
+		video.muted = true; 
+		document.getElementById("mute").innerHTML = "Unmute";
+	}
+	else{
+		video.muted = false;
+		document.getElementById("mute").innerHTML = "Mute";
+	}
+});
+
+document.querySelector("#slider").addEventListener("change", function() {
+	console.log(this.value)
+	var slider = this.value;
+	video.volume = slider / 100;
+	volume.innerHTML = video.volume * 100 + "%";
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	console.log("Vintage");
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	console.log("Original");
+	video.classList.remove("oldSchool");
+});
+
+
+
